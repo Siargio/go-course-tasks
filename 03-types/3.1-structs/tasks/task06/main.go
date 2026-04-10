aadd5c35dd915bd3faefbbac1f64a41dd7aae087
Siarheimasiuk <winst73@gmail.com>
2026-04-10 13:43:36 +0300

    3.1 task06 Две переиспользуемые части

--- 03-types/3.1-structs/tasks/task06/main.go
+++ 03-types/3.1-structs/tasks/task06/main.go
@@ -16,11 +16,33 @@ import "fmt"
 // TODO: объяви структуру Client с полями:
 //       ID (string), Address Address, и встроенным ContactInfo (embedding)
 
+type ContactInfo struct {
+	Phone string
+	Email string
+}
+
+type Address struct {
+	City   string
+	Street string
+}
+
+type Client struct {
+	ID      string
+	Address Address
+	ContactInfo
+}
+
 func main() {
 	// TODO: создай значение Client:
 	// ID: "C-01"
 	// Address: {City: "Moscow", Street: "Tverskaya"}
 	// ContactInfo: {Phone: "+7-999-000", Email: "ivan@example.com"}
+
+	c := Client{
+		ID:          "C-01",
+		Address:     Address{City: "Moscow", Street: "Tverskaya"},
+		ContactInfo: ContactInfo{Phone: "+7-999-000", Email: "ivan@example.com"},
+	}
 
 	fmt.Println("ID:", c.ID)
 	fmt.Println("City:", c.Address.City)

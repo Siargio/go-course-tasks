6064614cc24428c28c59dff51c381f03373c69ee
Siarheimasiuk <winst73@gmail.com>
2026-04-10 13:39:07 +0300

    3.1 task05 Композиция с Audit

--- 03-types/3.1-structs/tasks/task05/main.go
+++ 03-types/3.1-structs/tasks/task05/main.go
@@ -13,10 +13,25 @@ import "fmt"
 
 // TODO: объяви структуру Article с полем Title (string) и встроенным полем Audit (embedding)
 
+type Audit struct {
+	CreatedAt string
+	UpdatedAt string
+}
+
+type Article struct {
+	Title string
+	Audit
+}
+
 func main() {
 	// TODO: создай значение Article:
 	// Title: "Go Basics"
 	// Audit: {CreatedAt: "2024-01-01", UpdatedAt: "2024-06-01"}
+
+	a := Article{
+		Title: "Go Basics",
+		Audit: Audit{CreatedAt: "2024-01-01", UpdatedAt: "2024-06-01"},
+	}
 
 	fmt.Println("Title:", a.Title)
 	fmt.Println("CreatedAt:", a.CreatedAt)

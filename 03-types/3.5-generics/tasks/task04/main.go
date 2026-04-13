cb654e12145a2cffdd1e9ee546691363fe5b1a8b
Siarheimasiuk <winst73@gmail.com>
2026-04-13 19:32:41 +0300

    3.5 task04 Поиск индекса

--- 03-types/3.5-generics/tasks/task04/main.go
+++ 03-types/3.5-generics/tasks/task04/main.go
@@ -11,8 +11,24 @@ import "fmt"
 // TODO: напиши функцию IndexOf[T comparable](items []T, target T) int
 //       возвращает индекс target в items, или -1 если не найден
 
+func IndexOf[T comparable](items []T, target T) int {
+	var result int = -1
+
+	for i, v := range items {
+		if v == target {
+			result = i
+			return result
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	// TODO: вызови IndexOf([]string{"a", "b", "c"}, "c") и выведи "index: <результат>"
 	// TODO: вызови IndexOf([]string{"a", "b", "c"}, "z") и выведи "index: <результат>"
 	fmt.Println("TODO: implement me")
+
+	fmt.Println("index:", IndexOf([]string{"a", "b", "c"}, "c"))
+	fmt.Println("index:", IndexOf([]string{"a", "b", "c"}, "z"))
 }

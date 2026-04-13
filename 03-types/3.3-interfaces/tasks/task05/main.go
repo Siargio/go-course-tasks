5308b72532ad2c5eaad0c875479ec05388595e83
Siarheimasiuk <winst73@gmail.com>
2026-04-13 14:58:23 +0300

    3.3 task05 Безопасный type assertion

--- 03-types/3.3-interfaces/tasks/task05/main.go
+++ 03-types/3.3-interfaces/tasks/task05/main.go
@@ -6,12 +6,24 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // TODO: напиши функцию printStringLength(x any)
 //       используй безопасный type assertion (двузначная форма)
 //       если x — string, выводи "length: <len>"
 //       иначе выводи "not a string"
+
+func printStringLength(x any) {
+	s, ok := x.(string)
+
+	if ok {
+		fmt.Println("length:", len(s))
+	} else {
+		fmt.Println("not a string")
+	}
+}
 
 func main() {
 	printStringLength("hello")

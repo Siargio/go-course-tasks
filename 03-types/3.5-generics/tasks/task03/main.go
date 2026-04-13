ef641ebfb51ac5de5f535622dd91c56bec6f22f1
Siarheimasiuk <winst73@gmail.com>
2026-04-13 19:25:12 +0300

    3.5 task03 Сумма чисел

--- 03-types/3.5-generics/tasks/task03/main.go
+++ 03-types/3.5-generics/tasks/task03/main.go
@@ -13,8 +13,25 @@ import "fmt"
 // TODO: напиши функцию Sum[T Number](items []T) T
 //       возвращает сумму всех элементов среза
 
+type Number interface {
+	~int | ~float64
+}
+
+func Sum[T Number](items []T) T {
+	var result T
+	for _, item := range items {
+		result += item
+	}
+	return result
+}
+
 func main() {
 	// TODO: вызови Sum для []int{1, 2, 3, 4, 5} и выведи "sum int: <результат>"
 	// TODO: вызови Sum для []float64{1.5, 2.0, 4.0} и выведи "sum float: <результат>"
-	fmt.Println("TODO: implement me")
+
+	sumInt := Sum([]int{1, 2, 3, 4, 5})
+	fmt.Println("sum int:", sumInt)
+
+	sumFloat := Sum([]float64{1.5, 2.0, 4.0})
+	fmt.Println("sum int:", sumFloat)
 }

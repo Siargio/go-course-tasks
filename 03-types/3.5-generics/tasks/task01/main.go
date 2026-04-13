594f09cb3b91b36aa5b44283647359ef2c81df6d
Siarheimasiuk <winst73@gmail.com>
2026-04-13 18:26:16 +0300

    3.5 task01 Универсальный возврат

--- 03-types/3.5-generics/tasks/task01/main.go
+++ 03-types/3.5-generics/tasks/task01/main.go
@@ -12,8 +12,19 @@ import "fmt"
 // TODO: напиши функцию Echo[T any](v T) T
 //       она должна возвращать переданное значение без изменений
 
+func Echo[T any](v T) T {
+
+	return v
+}
+
 func main() {
 	// TODO: вызови Echo с int (42), string ("hello"), bool (true)
 	//       и выведи каждый результат через fmt.Println
-	fmt.Println("TODO: implement me")
+
+	echo := Echo([]any{42, "hello", true})
+
+	for _, result := range echo {
+		fmt.Println(result)
+	}
+
 }

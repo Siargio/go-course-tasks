f33112d82e1898e7c60cf1773aae25b660d17115
Siarheimasiuk <winst73@gmail.com>
2026-04-13 17:03:28 +0300

    3.4 task05 Оборачивание ошибки

--- 03-types/3.4-errors/tasks/task04/main.go
+++ 03-types/3.4-errors/tasks/task04/main.go
@@ -5,7 +5,10 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // TODO: напиши функцию readFileMock() error
 //       возвращай errors.New("file not found")
@@ -14,6 +17,19 @@ import "fmt"
 //       вызывает readFileMock() и оборачивает ошибку через fmt.Errorf с %w:
 //       "read file: <err>"
 //       сама тоже оборачивает: "load data: <err>"
+
+func readFileMock() error {
+	return errors.New("file not found")
+}
+
+func loadData() error {
+	err := readFileMock()
+	if err != nil {
+		wrapErr := fmt.Errorf("read file: %w", err)
+		return fmt.Errorf("load data: %w", wrapErr)
+	}
+	return nil
+}
 
 func main() {
 	err := loadData()
